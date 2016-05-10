e64c829bef142b1db4c1348597e25b6c698d0d60
Ruyu You <727809166@qq.com>
2016-05-10 11:01:42 +0800

    standard answers

--- Single Choice.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by you on 16-5-9.
- */
--- fill in the blank.js
+++ fill in the blank.js
@@ -1,3 +1,17 @@
 /**
  * Created by you on 16-5-9.
  */
+function loadFillInTheBlank() {
+    return [
+        {
+            answer:'统一建模语言'
+        },
+        {
+            answer:[
+                '封装性',
+                '继承性',
+                '多态性'
+            ]
+        }
+    ];
+}  
